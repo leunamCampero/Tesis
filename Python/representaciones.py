486e72289c1c0a3a48b89c233bf87a99168bf7a1
Rafael Villarroel <rvf0068@gmail.com>
2019-02-07 18:12:52 -0600

    No importar todo sympy

--- Python/representaciones.py
+++ Python/representaciones.py
@@ -1,5 +1,24 @@
-from sympy import Matrix
+import sympy
+
 from sympy.combinatorics.named_groups import SymmetricGroup
+from sympy.physics.quantum.dagger import Dagger
+
+
+def MTS(A):
+    """Calcula una matriz triangular superior"""
+    A1 = A
+    n = A.shape[0]
+    V = sympy.eye(n)
+    for i in range(0, n):
+        C = sympy.eye(n)
+        C[i, i] = 1/sympy.sqrt(A1[i, i])
+        for j in range(i+1, n):
+            C[i, j] = -(1/A1[i, i])*A1[i, j]
+        V = V*C
+        V.simplify()
+        A1 = Dagger(C)*A1*C
+        A1.simplify()
+    return V
 
 
 def characteristic_function(G, g, i, j):
@@ -15,13 +34,13 @@ def regular_representation(G):
     n = len(elems)
     mydict = {}
     for g in elems:
-        mydict[g] = Matrix(n, n,
-                           lambda i, j:
-                           characteristic_function(G, g, i, j))
+        mydict[g] = sympy.Matrix(n, n,
+                                 lambda i, j:
+                                 characteristic_function(G, g, i, j))
     return mydict
 
 
-H = SymmetricGroup(2)
+H = SymmetricGroup(3)
 
 repr = regular_representation(H)
 print(repr)
