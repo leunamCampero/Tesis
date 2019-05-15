09e93893160ec206515e5d0746f893d57b20e78e
Leunam Campero <leunam322jesms@hotmail.com>
2019-05-15 10:53:03 -0500

    Documenté algunas funciones para la clase MatrixRepresentation

--- Python/representations.py
+++ Python/representations.py
@@ -1555,13 +1555,56 @@ def Reduce(M):
     return A,M
 def _char_f(G, g, i, j):
     elems = list(G.elements)
-    if g*elems[i] == elems[j]:
+    if elems[i]*g == elems[j]:
         return 1
     else:
         return 0
 
 
 def regular_representation(G):
+    """Builds the regular representation.
+    
+    Args:
+        G (sympy.combinatorics.perm_groups.PermutationGroup): A symmetric group. 
+        
+    Returns:
+        __main__.MatrixRepresentation: The matrix regular representation.
+        
+    Examples:
+        To create the regular representation, you must have a symmetric group
+        and use ``regular representation(G)``.
+        
+        >>> G=SymmetricGroup(3)
+        >>> rr=regular_representation(G)
+        >>> display(rr.map)
+        ⎧     ⎡1  0  0  0  0  0⎤         ⎡0  0  0  0  1  0⎤            ⎡0  1  0  0  0  0⎤ 
+        ⎪     ⎢                ⎥         ⎢                ⎥            ⎢                |
+        ⎪     ⎢0  1  0  0  0  0⎥         ⎢0  0  0  0  0  1⎥            ⎢1  0  0  0  0  0|
+        ⎪     ⎢                ⎥         ⎢                ⎥            ⎢                |
+        ⎪     ⎢0  0  1  0  0  0⎥         ⎢0  0  0  1  0  0⎥            ⎢0  0  0  0  0  1|
+        ⎨(2): ⎢                ⎥, (1 2): ⎢                ⎥, (2)(0 1): ⎢                |,
+        ⎪     ⎢0  0  0  1  0  0⎥         ⎢0  0  1  0  0  0⎥            ⎢0  0  0  0  1  0|
+        ⎪     ⎢                ⎥         ⎢                ⎥            ⎢                |
+        ⎪     ⎢0  0  0  0  1  0⎥         ⎢1  0  0  0  0  0⎥            ⎢0  0  0  1  0  0|
+        ⎪     ⎢                ⎥         ⎢                ⎥            ⎢                |
+        ⎩     ⎣0  0  0  0  0  1⎦         ⎣0  1  0  0  0  0⎦            ⎣0  0  1  0  0  0⎦
+        
+                 ⎡0  0  0  1  0  0⎤           ⎡0  0  0  0  0  1⎤         ⎡0  0  1  0  0  0⎤⎫
+                 ⎢                ⎥           ⎢                ⎥         ⎢                ⎥⎪
+                 ⎢0  0  1  0  0  0⎥           ⎢0  0  0  0  1  0⎥         ⎢0  0  0  1  0  0⎥⎪
+                 ⎢                ⎥           ⎢                ⎥         ⎢                ⎥⎪
+                 ⎢0  0  0  0  1  0⎥           ⎢0  1  0  0  0  0⎥         ⎢1  0  0  0  0  0⎥⎪
+        (0 1 2): ⎢                ⎥, (0 2 1): ⎢                ⎥, (0 2): ⎢                ⎥⎪
+                 ⎢0  0  0  0  0  1⎥           ⎢1  0  0  0  0  0⎥         ⎢0  1  0  0  0  0⎥⎪
+                 ⎢                ⎥           ⎢                ⎥         ⎢                ⎥⎪
+                 ⎢0  1  0  0  0  0⎥           ⎢0  0  1  0  0  0⎥         ⎢0  0  0  0  0  1⎥⎪
+                 ⎢                ⎥           ⎢                ⎥         ⎢                ⎥⎪
+                 ⎣1  0  0  0  0  0⎦           ⎣0  0  0  1  0  0⎦         ⎣0  0  0  0  1  0⎦⎭
+         ..Note: In the first line of the code of this function is used ``G.elements``
+         which is a iterator, i.e every time the function ``regular_representation``
+         is run, the result could change, however the character is the same.
+             
+    """
     elems = list(G.elements)
     n = len(elems)
     mydict = {}
@@ -1570,61 +1613,114 @@ def regular_representation(G):
                                                  lambda i, j:
                                                  _char_f(G, g, i, j)))
     return MatrixRepresentation(mydict, G, n)
+def MTS(A):
+    """Calcula una matriz triangular superior"""
+    A1=A
+    n=A.shape[0]
+    V=eye(n)
+    for i in range(0,n):
+        C=eye(n)
+        C[i,i]=1/sqrt(A1[i,i])
+        for j in range(i+1,n):
+            C[i,j] = -(1/A1[i,i])*A1[i,j]
+        V=V*C
+        V.simplify()
+        A1=Dagger(C)*A1*C
+        A1.simplify()
+    return V
 
+def unitary_representation(G,d):
+    """Becomes a matrix representation in a unitary matrix representation.
+        
+    Args:
+        d (dict): A dict that must contains the map of a group into the group
+                of nonsingular linear transformations of some finite dimensional
+                vector space.
+        G (sympy.combinatorics.perm_groups.PermutationGroup): The group. 
+        
+    Returns:
+        __main__.MatrixRepresentation: A unitary matrix representation.
 
-def unitary_representation(G, d):
     """
-    A function that given a matrix representation become it
-    in a unitary matrix representation.
-    """
-    n = d.degree
-    A = sp.zeros(n, n)
+    n=d.degree
+    A=zeros(n,n)
     for g in d.map:
-        J = (d.map[g].H)*d.map[g]
-        J = sp.expand(J)
-        A = J+A
-    A1 = A
-    V = sp.eye(n)
-    for i in range(0, n):
-        C = sp.eye(n)
-        C[i, i] = sp.S(1)/sp.sqrt(A1[i, i])
-        for j in range(i+1, n):
-            C[i, j] = -(1/A1[i, i])*A1[i, j]
-        V = V*C
-        V = sp.expand(V)
-        A1 = (C.H)*A1*C
-        A1 = sp.expand(A1)
+        J=(d.map[g].H)*d.map[g]
+        J=expand(J)
+        A=J+A
+    A1=A
+    V=eye(n)
+    for i in range(0,n):
+        C=eye(n)
+        C[i,i]=1/sqrt(A1[i,i])
+        for j in range(i+1,n):
+            C[i,j] = -(1/A1[i,i])*A1[i,j]
+        V=V*C
+        V=expand(V)
+        A1=(C.H)*A1*C
+        A1=expand(A1)
+    V=MTS(A)
     M = {}
     for g in list(G.elements):
-        M[g] = sp.ImmutableMatrix((V.inv())*d.map[g]*V)
+        M[g]=sp.ImmutableMatrix((V.inv())*d.map[g]*V)
     return MatrixRepresentation(M, G, n)
 
+def is_irreducible(G,d):
+    """Determines if a representation is irreducible.
+    
+        Args:
+            d (dict): A dict that must contains the map of a group into the group
+                of nonsingular linear transformations of some finite dimensional
+                vector space.
+                
+            G (sympy.combinatorics.perm_groups.PermutationGroup): The group. 
+        
+        Returns:
+            True if the representation is irreducible, a matrix non 
+            escalar that reduce the matrix representation in otherwise.
 
-def is_irreducible(G, d):
-    """A function that determine if a matrix representation is irreducible,
-    if is reducible, return a matrix non escalar that reduce the
-    matrix representation in otherwise return True"""
-    n = d.degree
-    N = sp.eye(n)
-    for r in range(0, n):
-        for s in range(0, n):
-            H = sp.zeros(n)
-            if (n-1-r == n-1-s):
-                H[n-1-r, n-1-r] = 1
+        Examples:
+            To see if a representation is irreducible use
+            ``is_irreducible(G,d)``.
+            
+            >>> G=SymmetricGroup(3)
+            >>> rr=regular_representation(G)
+            >>> M=is_irreducible(G,rr)
+            >>> M
+            ⎡ 0    0   1/3   0    0    0 ⎤
+            ⎢                            ⎥
+            ⎢ 0    0    0   1/3   0    0 ⎥
+            ⎢                            ⎥
+            ⎢1/3   0    0    0    0    0 ⎥
+            ⎢                            ⎥
+            ⎢ 0   1/3   0    0    0    0 ⎥
+            ⎢                            ⎥
+            ⎢ 0    0    0    0    0   1/3⎥
+            ⎢                            ⎥
+            ⎣ 0    0    0    0   1/3   0 ⎦
+            
+    """
+    n=d.degree
+    N=eye(n)
+    for r in range(0,n):
+        for s in range(0,n):
+            H=zeros(n)        
+            if (n-1-r==n-1-s):
+                H[n-1-r,n-1-r]=1
             else:
-                if (n-1-r > n-1-s):
-                    H[n-1-r, n-1-s] = 1
-                    H[n-1-s, n-1-r] = 1
+                if (n-1-r>n-1-s):
+                    H[n-1-r,n-1-s]=1
+                    H[n-1-s,n-1-r]=1
                 else:
-                    H[n-1-r, n-1-s] = 1*sp.I
-                    H[n-1-s, n-1-r] = -1*sp.I
-            M = sp.zeros(n, n)
-            R = unitary_representation(G, d)
+                    H[n-1-r,n-1-s]=1*I
+                    H[n-1-s,n-1-r]=-1*I
+            M=zeros(n,n)
+            R=unitary_representation(G,d)
             for g in R.map:
-                M = M+(R.map[g].H*H*R.map[g])
-            M = (sp.S(1)/n)*M
-            M = sp.expand(M)
-            if (M != M[0, 0]*N):
+                M=M+(R.map[g].H*H*R.map[g])
+            M=(sympify(1)/n)*M
+            M=expand(M)
+            if (M!=M[0,0]*N):
                 return M
     else:
         return True
@@ -1632,74 +1728,70 @@ def is_irreducible(G, d):
 
 def block(M):
     """A function that return where end the blocks of a matrix."""
-    v = []
-    c1 = 0
-    i = 0
-    n = M.shape[0]
-    while (c1 < n):
-        c = 0
-        for j in range(c1, n):
-            if (M[i, j] != 0 or M[j, i] != 0):
-                if (sp.Abs(i-j) > c):
-                    c = sp.Abs(i-j)
-        if (c == 0):
+    v=[]
+    c1=0
+    i=0
+    n=M.shape[0]
+    while (c1<n):
+        c=0
+        for j in range(c1,n):
+            if (M[i,j]!=0 or M[j,i]!=0):
+                if (Abs(i-j)>c):
+                    c=Abs(i-j)
+        if (c==0):
             v.append(c1)
-            c1 = c1+1
-            i = c1
+            c1=c1+1
+            i=c1
         else:
             bloques=False
             while (bloques==False):
                 bloques=True
                 for j in range(c1,c1+c+1):
                     for k in range(c1+c+1,n):
-                        if (M[j,k]!=0 or M[k,j]!=0):
-                            bloques=False
-                            c=Abs(i-k)
+                         if (M[j,k]!=0 or M[k,j]!=0):
+                            if (Abs(i-k)>c):
+                                c=Abs(i-k)
             v.append(c1+c)
             c1=c1+c+1
             i=c1
     return v
 
-def blockI(M, n, i):
-    """A function that given a matrix, put it since the entry (i,i) of a
+def blockI(M,n,i):
+    """A function that given a matrix, put it since the entry (i,i) of a 
     identity matrix of degree n"""
-    a = M.shape[0]
-    N = sp.eye(n)
-    for j in range(0, a):
-        for k in range(0, a):
-            N[j+i, k+i] = M[j, k]
+    a=M.shape[0]
+    N=eye(n)
+    for j in range(0,a):
+        for k in range(0,a):
+            N[j+i,k+i]=M[j,k]
     return N
 
-
-def reduce(G, d):
+def reduce(G,d):
     """This function give a matrix that reduce the matrix representation"""
-    M = is_irreducible(G, d)
-    b = d.degree
-    if M is True:
-        return(sp.eye(b))
+    M=is_irreducible(G,d)
+    b=d.degree
+    if (M==True):
+        return(eye(b))
     else:
         (P, J) = M.jordan_form()
-        P = sp.expand(P)
-        w = []
+        P=expand(P)
+        w=[]
         for g in d.map:
             w.append(block(P.inv()*d.map[g]*P))
-        lon = len(w[0])
-        au = w[0]
+        l=len(w[0])
+        au=w[0]
         for g in w:
-            if (len(g) < lon):
-                lon = len(g)
-                au = g
-        e = 0
-        U = P
+            if (len(g)<l):
+                l=len(g)
+                au=g
+        e=0
+        U=P
         for a in au:
-            d1 = {}
+            d1={}
             for g in list(G.elements):
-                d1[g] = sp.ImmutableMatrix((P.inv()*d.map[g]*P)[e:a+1, e:a+1])
-            if (is_representation(G,MatrixRepresentation(d1, G, (a+1-e)))==True):
-                U = U*blockI(reduce(G, MatrixRepresentation(d1, G, (a+1-e))), b, e)
-            else:
-                display("Map d1 does not meet the requirements to be a representation.")
-            e = a+1
+                d1[g]=sp.ImmutableMatrix((P.inv()*d.map[g]*P)[e:a+1,e:a+1])
+            U=U*blockI(reduce(G,MatrixRepresentation(d1,G,(a+1-e))),b,e)
+            e=a+1
         return U
 
     
